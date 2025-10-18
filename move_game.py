b98a02de78a693543013310ef08deedcba514d79
seyonbs <seyonbs@gmail.com>
2025-10-18 16:03:43 -0400

    Add files via upload

--- move_game.py
+++ move_game.py
@@ -3,8 +3,13 @@ pygame.init()
 screen = pygame.display.set_mode((1000, 1000))
 speed = 7
 character_rect = pygame.Rect((250, 150, 50, 50))
+character_rect.x = 2
+character_rect.y = 2
 running = True
 while running:
       keys = pygame.key.get_pressed()
-      if character_rect[keys[pygame.K_UP]]:
-        pass
+      if keys[pygame.K_UP]:
+            character_rect.y +=1
+      if keys[pygame.K_DOWN]:
+            character_rect.y -=1
+                   
