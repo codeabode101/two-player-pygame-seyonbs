86d9d9ca8986b18f7e6f3d4af49c3ff36f6cb970
seyonbs <seyonbs@gmail.com>
2025-10-20 20:16:03 -0400

    Add files via upload

--- move_game.py
+++ move_game.py
@@ -23,7 +23,7 @@ while running:
             character_rect.x += speed
       
       screen.fill((165,234,7))
-      pygame.draw.rect(screen, (180,180,180),character_rect)
+      pygame.draw.rect(screen, (180,180,180),character_rect,2)
       speed = 1
       character_rect_2 = pygame.Rect((250, 150, 50, 50))
       character_rect_2.x = 270
@@ -40,6 +40,10 @@ while running:
             character_rect_2.x -= speed
       if keys[pygame.K_a]:
             character_rect_2.x += speed
+      if character_rect.x < 0:
+            character_rect.x = 0
+      if character_rect.x > 0:
+            character_rect = 0          
       pygame.draw.rect(screen, (180,180,180),character_rect_2)      
       pygame.display.flip()                             
 pygame.quit()                   
